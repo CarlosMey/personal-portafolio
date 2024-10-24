cb928341f2e0de462878af67560d0ea9c74fc0a3
Carlos Mey <meycar96@gmail.com>
2024-10-23 21:58:47 -0500

    agregado linkedIn y facebook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 // import logo from '../assets/img/logo.svg';
-// import navIcon1 from '../assets/img/nav-icon1.svg';
-// import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
 // import navIcon3 from '../assets/img/nav-icon3.svg';
 // import { HashLink } from 'react-router-hash-link';
 // import {
@@ -50,9 +50,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                {/* <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a> */}
+                <a href="https://www.linkedin.com/in/carlos-mey-ramos-b1258322b/"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.facebook.com/carlos.mey.14/?locale=es_LA"><img src={navIcon2} alt="" /></a>
+                {/* <a href="#"><img src={navIcon3} alt="" /></a> */}
               </div>
               {/* <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
